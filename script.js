2bca224719152cb1c49d8d1bad9090f60c95d2e6
ozymok1604 <ozimok1604ua@gmail.com>
2022-03-19 21:43:06 +0200

    second commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const students = [
     course: 3,
     subjects: {
       math: [4, 4, 3, 4],
-      algorithms: [3, 3, 3, 4, 4, 4],
+      algorithms: [3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5],
       data_science: [5, 5, 3, 4],
     },
   },
@@ -28,33 +28,30 @@ const students = [
   },
 ];
 
-function getSubjects() {
-  let lessons = [];
-  for (i = 0; i < students.length; i++) {
-    lessons = lessons.concat(Object.keys(students[i].subjects));
-  }
-
-  const getUpper = () => {
-    return lessons.map((lesson) => {
-      let word = lesson.split("");
-      if (word.indexOf("_") != -1) {
-        word[word.indexOf("_")] = " ";
-        word[0] = word[0].toUpperCase();
-        console.log(word.join(""));
-      } else {
-        console.log(lesson[0].toUpperCase() + lesson.slice(1));
-      }
-    });
-  };
-  getUpper();
+function getSubjects(student) {
+  students.forEach((person) => {
+    if (person.name === student) {
+      data = Object.keys(person.subjects);
+      data.map((name) => {
+        let word = name.split("");
+        if (word.indexOf("_") != -1) {
+          word[word.indexOf("_")] = " ";
+          word[0] = word[0].toUpperCase();
+          console.log(word.join(""));
+        } else {
+          console.log(name[0].toUpperCase() + name.slice(1));
+        }
+      });
+    }
+  });
 }
 
-//getSubjects();
+//getSubjects("Tanya");
 
 const getAvarage = (student) => {
   let marks = [];
   let sum = 0;
-   students.map((person) => {
+  students.forEach((person) => {
     if (person.name === student) {
       let arrays = person.subjects;
       for (key in arrays) {
@@ -74,50 +71,47 @@ const getAvarage = (student) => {
 const getInformation = (student) => {
   students.map((person) => {
     if (person.name === student) {
-      console.log("course:", person.course);
-      console.log("name:", person.name);
-      console.log("AvarageMark:", getAvarage(student));
+      info = {
+        course: person.course,
+        name: person.name,
+        AvarageMark: getAvarage(student),
+      };
+      console.log(info);
     }
   });
 };
 
 //getInformation("Tanya");
 
-const getSortedNames = () => students.map((student) =>student.name).sort();
+const getSortedNames = () => students.map((student) => student.name).sort();
 
 //console.log(getSortedNames());
 
-const getBestStudent = () => {
-  names = [];
-  numbers = [];
-  max = 0;
-  students.map((person) => {
-    let mark = getAvarage(person.name);
-    numbers.push(mark);
-    names.push(person.name);
-    if (mark > max) {
-      max = mark;
-    }
-  });
-  for (i = 0; i < names.length; i++) {
-    if (max === numbers[i]) {
-      console.log(names[i]);
-    }
-  }
+
+const getLettersNumbers = (word) => {
+  word = word.split("");
+  const newObj = word.reduce((acc, el) => {
+    if (acc[el]) {
+      return { ...acc, [el]: (acc[el] += 1) };
+    } else return { ...acc, [el]: 1 };
+  }, {});
+  console.log(newObj);
+  return newObj;
 };
-//getBestStudent();
 
+//getLettersNumbers('paaassword');
 
-const getLettersNumbers = (word) =>{
-     word = word.split('');
-     const newObj = word.reduce((acc, el)=>{
-       if(acc[el]){
-           return {...acc, [el]: acc[el]+=1}
-       }
-      else return {...acc, [el]: 1}
-     }, {})
-console.log(newObj)
-     return newObj;
+function getBestStudent() {
+  let data = [];
+  students.forEach((person) => {
+    data.push({ name: person.name, mark: getAvarage(person.name) });
+  });
+
+  function sortByMark(data) {
+    data.sort((a, b) => (a.mark < b.mark ? 1 : -1));
+  }
+  sortByMark(data);
+  console.log(data[0]);
 }
 
-getLettersNumbers('paaassword');
+getBestStudent();
